87a01dfe3d0726a4a44a1efaeaea9e4dbc29a9e4
Nikhar Saxena <nikhar.saxena@sentry.io>
2024-08-28 13:45:48 -0700

    Chapter 2

--- sample-express/index.js
+++ sample-express/index.js
@@ -1,9 +1,23 @@
-const express = require('express')
+import { routeHello, routeAPINames } from "./routes.js";
+import express from "express";
+
 const server = express();
 const port = 3000;
 
 server.get('/hello', (req, res) => {
-    res.send('Hello World!');
+    const response = routeHello(req, res);
+    res.send(response);
+});
+
+server.get('/api/names', async function (req, res) {
+    let response;
+    try {
+        response = await routeAPINames(req, res);
+    } catch (err) {
+        console.log(err);
+    }
+
+    res.send(response);
 });
 
 server.listen(port, () => {
