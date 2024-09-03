8ac5dc494b155a6c2733b54007f4bde5feb887da
Nikhar Saxena <nikhar.saxena@sentry.io>
2024-09-03 16:44:57 -0700

    Build the app with next

---
+++ sample-next/my-app/pages/hello.tsx
@@ -0,0 +1,29 @@
+import type {NextPage} from "next";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+const Hello: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>Hello World Page Title</title>
+                <meta property="og:title" content="Hello World" key="title" />
+            </Head>
+            <div>Hello World!</div>
+            <div>
+                Use the HTML anchor for an <a href="https://nostarch.com">
+                external link</a> and the Link component for an
+                <Link href="/components/weather"> internal page</Link>.
+                <Image
+                    src="/vercel.svg"
+                    alt="Vercel Logo"
+                    width={72}
+                    height={16}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Hello;
